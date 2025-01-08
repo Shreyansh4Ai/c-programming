357f756df2be5b4baf7d4b1055f4d121747bcd62
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-01-08 15:01:01 +0530

    Add files via upload

---
+++ n natural numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("first n natural no.s are:");
+    scanf("%d\n",&n);
+    for(i=1;i<=n;i++)
+    printf("\n");
+    printf("%d",i);
+    return 0;
+    
+}
