e9a5b8a4213c698ae1c22e83cdc0cc22c3e32f66
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-02-21 11:27:12 +0530

    Create basics.c

---
+++ pattern printing/basics.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+ for(int i=0;i<7;i++){
+    for(int j=7;j<5;j--){
+        printf("%d ", arr[i]);
+    }
+ }
+    
+        
+    
+    
+
+
+    return 0;
+}
